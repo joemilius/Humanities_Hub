7d9233b4ae3a4bf2fd0c7b3f3f0a121358c3e8ea
JoeMilius <joemilius@gmail.com>
2021-10-05 23:16:53 -0600

    switch and routes put in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      
+      <Switch>
+        <Route></Route>
+      </Switch>
+      <Switch>
+        <Route></Route>
+      </Switch>
+      <Switch>
+        <Route></Route>
+      </Switch>
+      <Switch>
+        <Route></Route>
+      </Switch>
     </Router>
   );
 }
