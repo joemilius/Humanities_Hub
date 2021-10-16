45572c94072c29ddbe3cf4906928db215564f19c
JoeMilius <joemilius@gmail.com>
2021-10-16 02:54:39 -0600

    added brand text to navbar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {NavBar, Container, Nav, Button} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <NavBar classname="navbar" bg="light" expand="lg" variant="light">
-            
+            <Container>
+                <NavBar.Brand>
+                    <NavLink classname="navlinks" to="/"><h2 classname="brand-text">HumanitiesHub</h2></NavLink>
+                </NavBar.Brand>
+            </Container>
         </NavBar>
     )
 }
