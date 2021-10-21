1a16b4449e4e673faf290858d35a78ef696f3086
JoeMilius <joemilius@gmail.com>
2021-10-21 00:53:57 -0600

    states set for queries in search bars

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from 'react-bootstrap'
 
 const Home = () => {
+    const [movieQuery, setMovieQuery] = useState('')
+    const [musicQuery, setMusicQuery] = useState('')
+    const [bookQuery, setBookQuery] = useState('')
+    
     return (
         <div>
         <Form>
             <Form.Label>Movie Search</Form.Label>
-            <Form.Control classname='inputbox'></Form.Control>
+            <Form.Control classname='inputbox' onChange={}></Form.Control>
         </Form>
         <Form>
             <Form.Label>Music Search</Form.Label>
-            <Form.Control classname='inputbox'></Form.Control>
+            <Form.Control classname='inputbox' onChange={}></Form.Control>
         </Form>
         <Form>
             <Form.Label>Book Search</Form.Label>
-            <Form.Control classname='inputbox'></Form.Control>
+            <Form.Control classname='inputbox' onChange={}></Form.Control>
         </Form>
         </div>
     )
