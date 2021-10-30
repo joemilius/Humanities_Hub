c00e51258bddf02095b63b2b8bb060df05beede1
JoeMilius <joemilius@gmail.com>
2021-10-30 17:04:01 -0600

    created state for login values

--- client/src/Users/LoginForm.js
+++ client/src/Users/LoginForm.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from 'react-bootstrap'
 
 const LoginForm = () => {
+    const {loginData, setLoginData} = useState({
+        username: '',
+        password: ''
+    })
+
     return (
         <Form>
             <Form.Group>
                 <Form.Label>Username</Form.Label>
-                <Form.Control className="inputbox" name="username" value={}></Form.Control>
-                <Form.Label></Form.Label>
-                <Form.Control className="inputBox" type="password" name="password" value={}></Form.Control>
+                <Form.Control className="inputbox" name="username" value={loginData.username}></Form.Control>
+                <Form.Label>Password</Form.Label>
+                <Form.Control className="inputBox" type="password" name="password" value={loginData.password}></Form.Control>
             </Form.Group>
         </Form>
     )
