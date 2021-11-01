7f1ce2e3464b96dfe8dcb43d6ffe21de240ce11a
JoeMilius <joemilius@gmail.com>
2021-10-31 21:46:42 -0600

    signup form inputs labeled and state established

--- client/src/Users/SignUpForm.js
+++ client/src/Users/SignUpForm.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from 'react-bootstrap'
 
 const SignUpForm = () => {
+    const [signupData, setSignupData] = usestate({
+        first_name: '',
+        last_name: '',
+        username: '',
+        email: '',
+        password: ''
+    })
     return (
         <Form>
             <Form.Group>
-                <Form.Label></Form.Label>
-                <Form.Control></Form.Control>
-                <Form.Label></Form.Label>
-                <Form.Control></Form.Control>
-                <Form.Label></Form.Label>
-                <Form.Control></Form.Control>
-                <Form.Label></Form.Label>
-                <Form.Control></Form.Control>
-                <Form.Label></Form.Label>
-                <Form.Control></Form.Control>
+                <Form.Label>First Name</Form.Label>
+                <Form.Control classname="inputbox" name="first_name"></Form.Control>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control classname="inputbox" name="last_name"></Form.Control>
+                <Form.Label>Username</Form.Label>
+                <Form.Control classname="inputbox" name="username"></Form.Control>
+                <Form.Label>Email</Form.Label>
+                <Form.Control classname="inputbox" name="email"></Form.Control>
+                <Form.Label>Password</Form.Label>
+                <Form.Control classname="inputbox" name="password"></Form.Control>
             </Form.Group>
         </Form>
     )
