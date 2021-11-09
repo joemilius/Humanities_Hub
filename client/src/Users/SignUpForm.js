1218ef34ce3ac9f7abfc78c2bab5f67914d76a4b
JoeMilius <joemilius@gmail.com>
2021-11-09 09:06:57 -0700

    control values created for signup form

--- client/src/Users/SignUpForm.js
+++ client/src/Users/SignUpForm.js
@@ -5,13 +5,13 @@ const SignUpForm = () => {
     const [signupData, setSignupData] = usestate({
         first_name: '',
         last_name: '',
-        username: '',
         email: '',
+        username: '',
         password: ''
     })
 
     function handleSignup(event) {
-        setSignupData({...loginData,
+        setSignupData({...signupData,
             [event.target.name] : event.target.value
         })
     }
@@ -21,16 +21,17 @@ const SignUpForm = () => {
         <Form onSubmit={}>
             <Form.Group>
                 <Form.Label>First Name</Form.Label>
-                <Form.Control classname="inputbox" name="first_name" onChange={handleSignup}></Form.Control>
+                <Form.Control classname="inputbox" name="first_name" value={signupData.first_name} onChange={handleSignup}></Form.Control>
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control classname="inputbox" name="last_name" onChange={handleSignup}></Form.Control>
-                <Form.Label>Username</Form.Label>
-                <Form.Control classname="inputbox" name="username" onChange={handleSignup}></Form.Control>
+                <Form.Control classname="inputbox" name="last_name" value={signupData.last_name} onChange={handleSignup}></Form.Control>
                 <Form.Label>Email</Form.Label>
-                <Form.Control classname="inputbox" name="email" onChange={handleSignup}></Form.Control>
+                <Form.Control classname="inputbox" name="email" value={signupData.email} onChange={handleSignup}></Form.Control>
+                <Form.Label>Username</Form.Label>
+                <Form.Control classname="inputbox" name="username" value={signupData.username} onChange={handleSignup}></Form.Control>
                 <Form.Label>Password</Form.Label>
-                <Form.Control classname="inputbox" name="password" onChange={handleSignup}></Form.Control>
+                <Form.Control classname="inputbox" name="password" value={signupData.password} onChange={handleSignup}></Form.Control>
             </Form.Group>
+            <Button variant='custom' type='submit'>SignUp</Button>
         </Form>
     )
 }
