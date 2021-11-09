de94e1f6e024ce7abbaf34f16812e55f7a913522
JoeMilius <joemilius@gmail.com>
2021-11-09 09:24:06 -0700

    imported button from bootstrap in loginform and signup form

--- client/src/Users/LoginForm.js
+++ client/src/Users/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Form from 'react-bootstrap'
+import {Form, Button} from 'react-bootstrap'
 
 const LoginForm = ({}) => {
     const {loginData, setLoginData} = useState({
