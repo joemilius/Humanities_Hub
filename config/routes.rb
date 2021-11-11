8f563d9f70e70ca46fb95214564d15b695a0d669
JoeMilius <joemilius@gmail.com>
2021-11-11 16:25:25 -0700

    created path to sessions destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
