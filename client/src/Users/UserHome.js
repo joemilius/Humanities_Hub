d2bcf2d531335f786dee9e87aa1e6c2760d3fbb0
JoeMilius <joemilius@gmail.com>
2021-11-15 16:22:46 -0700

    added form for creating a new group

--- client/src/Users/UserHome.js
+++ client/src/Users/UserHome.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
-import Button from 'react-bootstrap'
+import {Button, Form} from 'react-bootstrap'
 
 const UserHome = ({user, allPublicGroups, setAllPublicGroups}) => {
     const [allUsers, setAllUsers] = useState([])
+    const [createGroup, setCreateGroup] = useState({
+        group_name: ''
+    })
     
     function getPublicGroups(){
         fetch(`/groups`)
@@ -17,6 +20,21 @@ const UserHome = ({user, allPublicGroups, setAllPublicGroups}) => {
         fetch(`http://localhost:3000/users`)
         .then(response => response.json())
         .then(data => setAllUsers(data))
+    }
+
+    function handleCreateGroup(){
+        
+    }
+
+    function createGroup(){
+        fetch(`http://localhost:3000/groups`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(createGroup)
+        })
+
     }
     return (
         <div>
@@ -45,6 +63,13 @@ const UserHome = ({user, allPublicGroups, setAllPublicGroups}) => {
                     </div>
                 )
             })}
+            </div>
+            <div>
+                <Form>
+                    <Form.Label>Group Name</Form.Label>
+                    <Form.Control classname='inputbox' name='groupname' value={handleCreateGroup} ></Form.Control>
+                    <Button onClick={}>Create Group</Button>
+                </Form>
             </div>
 
             {groups ?
